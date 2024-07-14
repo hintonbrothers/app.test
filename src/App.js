c356d5641d3ced7db9065250e21f09bd647fa681
unknown <hintonbrothersprivatelimited@gmail.com>
2024-07-14 19:57:11 +0530

    commiting test app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
     return (<div className="App">
         <header className="App-header">
-            <h2>TestApp Testing1</h2>
+            <h2>TestApp Testing1.</h2>
             <h2>TestApp Testing2.</h2>
             <h2>TestApp Testing3.</h2>
             <h2>TestApp Testing4..</h2>
