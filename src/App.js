876ac102db4e544ff326c212821b67d4627b394e
unknown <hintonbrothersprivatelimited@gmail.com>
2024-07-14 18:46:30 +0530

    commiting test app

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ function App() {
     return (<div className="App">
         <header className="App-header">
             <h2>TestApp Testing1</h2>
-            <h2>TestApp Testing2</h2>
-            <h2>TestApp Testing3</h2>
-            <h2>TestApp Testing4</h2>
-            <h2>TestApp Testing5</h2>
+            <h2>TestApp Testing2.</h2>
+            <h2>TestApp Testing3.</h2>
+            <h2>TestApp Testing4..</h2>
+            <h2>TestApp Testing5...</h2>
         </header>
     </div>);
 }
